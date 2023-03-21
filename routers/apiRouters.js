bfa521f9a7f093b9ca875b0e17d087ccc4b91250
Develokev <develokev@gmail.com>
2023-03-22 00:17:29 +0100

    dashboard Route, Render, EJS done. CSS Initiallized. Need to check routes to views

--- routers/apiRouters.js
+++ routers/apiRouters.js
@@ -4,4 +4,5 @@ const { getMovie } = require("../controllers/apiControllers");
 
 router.get("/movies", getMovie);
 
+
 module.exports = router;
---
+++ routers/userRouters.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const { showDashboard } = require('../controllers/userControllers')
+
+//DASHBOARD renderView
+router.get("/dashboard", showDashboard);
