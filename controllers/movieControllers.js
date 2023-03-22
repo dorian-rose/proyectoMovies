ae22e8d259227b38c3d03cb932520c43baa55d3f
santism98 <santiagosuarezmanso@gmail.com>
2023-03-22 16:18:54 +0100

    buscar fotos bien, falta enlazar formulario

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -1,8 +1,24 @@
-const getTest = (req, res) => {
-    res.render("userViews/testPage", {
-        titulo: "Test Page",
-        texto: "This is a description of test page",
+const { consultation } = require('../helpers/fetch');
+
+const getMovie = async (req, res) => {
+  const searchQuery = req.query.search;
+  
+  try {
+    const response = await consultation(`https://www.omdbapi.com/?apikey=cf8ab226&s=${searchQuery}`); //mirar template
+    const movies = response.Search;
+    
+    
+    console.log(movies); // Imprime los resultados en la consola
+    
+    res.render('movies', { movies });
+  } catch (error) {
+    console.error(error);
+    
+    return res.status(500).json({
+      ok: false,
+      msg: 'Error retrieving movies',
     });
+  }
 };
 
-module.exports = { getTest }
+module.exports = { getMovie };
--- routers/movieRouters.js
+++ routers/movieRouters.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {
-    getTest
+const {getMovie
+    
 } = require("../controllers/movieControllers");
 
-router.get("/test", getTest);
+router.get("/movies", getMovie);
 
 module.exports = router;
