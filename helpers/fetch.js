98c588b6eb309c0ebd3cd616db7e623748f215e9
santism98 <santiagosuarezmanso@gmail.com>
2023-03-29 11:13:29 +0200

    fetch nuevo

---
+++ helpers/fetch.js
@@ -0,0 +1,37 @@
+const consultation = async (url, method, body = {}) => {  //! ver qué modificar
+    let options = {};
+       // console.log('estos son los parametros de la consulta',{url},{method},{body})
+    const data = { ...body }
+   // if (data.title) {
+   //     const titleSpaced = data.title.replaceAll("_", " ")
+    //    data.title = titleSpaced.toLowerCase()
+  //  }
+
+    try {
+    console.log('estamos entrando al try del fetch')
+        if (method == "POST" || method == "PUT" || method == "DELETE") {
+
+            options = {
+                method: method,
+                body: JSON.stringify(data),
+                headers: {
+                    "Content-type": "application/json",
+                }
+            }
+        }
+
+
+        // console.log('esta es la resp del GET', `${url}`)
+        let respuesta= await fetch(`${url}`, options);
+        
+        let resp = await respuesta.json();
+        
+        return resp;
+
+    } catch (error) {
+        console.log('FAILED while fetching', error)
+    }
+}
+
+
+module.exports = { consultation };
